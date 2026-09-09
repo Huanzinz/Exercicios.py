a6e8b9855eef3cb2a6e5e146416baff86a412d23
Huanzinz <huanrodriguesz@gmail.com>
2026-09-08 21:14:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-echo "# Exercicios.py" >> README.md 
-git init 
-git add README.md 
-git commit -m "primeiro commit" 
-git branch -M main 
-git remote add origin https://github.com/Huanzinz/Exercicios.py.git
- git push -u origin main
+
